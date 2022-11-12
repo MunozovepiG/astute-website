fbf6aaee74830038f4f89eef6f0a7cd8e3fff457
Consultant - Gwata Munozovepi <munozovepi@astute.money>
2022-11-12 08:02:47 +0400

    added the navigation and completed the pages, fully responsive for the pages relating to the about-links-section

--- src/app/components/round-button-default/round-button-default.component.ts
+++ src/app/components/round-button-default/round-button-default.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-round-button-default',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./round-button-default.component.scss']
 })
 export class RoundButtonDefaultComponent implements OnInit {
+  @Input() color:string | undefined
+  @Input() text:string | undefined
+
+  
 
   constructor() { }
 
